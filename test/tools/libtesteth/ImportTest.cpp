14676fa9884cb9b40d252df150af19fcdfba2158
Dimitry <dimitry@ethereum.org>
2017-12-11 21:54:32 +0300

    try fixing travis dot insert into state root debug

--- test/tools/libtesteth/ImportTest.cpp
+++ test/tools/libtesteth/ImportTest.cpp
@@ -226,8 +226,7 @@ std::tuple<eth::State, ImportTest::ExecOutput, eth::ChangeLog> ImportTest::execu
 			st.setShowMnemonics();
 			st.setOptions(Options::get().jsontraceOptions);
 			out = initialState.execute(_env, *se.get(), _tr, Permanence::Committed, st.onOp());
-			cout << st.json();
-			cout << "{\"stateRoot\": \"" << initialState.rootHash().hex() << "\"}";
+			cout << st.json() + "{\"stateRoot\": \"" + initialState.rootHash().hex() + "\"}";
 		}
 		else
 			out = initialState.execute(_env, *se.get(), _tr, Permanence::Uncommitted);
